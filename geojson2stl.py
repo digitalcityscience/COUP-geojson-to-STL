7a7622c57bb556c0a65260b8adc9476b171400cc
Andre@HCU <andre.landwehr@hcu-hamburg.de>
2023-10-02 11:33:10 +0200

    feat: cut out interior rings. cut out placeholder marker

--- geojson2stl.py
+++ geojson2stl.py
@@ -30,12 +30,15 @@ def read_geojson_files() -> geopandas.GeoDataFrame():
     return pd.concat(gdfs)
 
 
-def cad_from_coords(workplane, coordinates, extrude_by=0):
+def solid_from_coords(workplane, coords_exterior, cut_outs=None, extrude_by=0):
     # create a cad object from geometry
 
-    print(coordinates)
-    exit()
-    result = workplane.polyline(coordinates).close()
+    print(coords_exterior)
+    # exit()
+    result = workplane.polyline(coords_exterior).close()
+
+    for cut_out in cut_outs:
+        result = result.polyline(cut_out).close()
 
     if extrude_by:
         result = result.extrude(extrude_by)
@@ -48,7 +51,9 @@ def make_cq_workplane_with_building(building: geopandas.GeoDataFrame()):
     building_centroid = (
         building[building["building_part"] == "base"].iloc[0].geometry.centroid
     )
-    building["geometry"] = building.translate(-building_centroid.x, -building_centroid.y)
+    building["geometry"] = building.translate(
+        -building_centroid.x, -building_centroid.y
+    )
 
     workplane = cq.Workplane("XY")
 
@@ -56,26 +61,31 @@ def make_cq_workplane_with_building(building: geopandas.GeoDataFrame()):
         print(building_part)
         # read geometry from geojson
         gdf = building[building["building_part"] == building_part]
+
         if len(gdf.index) == 0:
             continue
 
         geojson = json.loads(gdf.to_json())
-        coordinates = geojson["features"][0]["geometry"]["coordinates"][0]  # exterior
-        # TODO truncate Z values!
-        # TODO: cut interiors!
+        coords_exterior_ring = geojson["features"][0]["geometry"]["coordinates"][0]
+        interior_rings = geojson["features"][0]["geometry"]["coordinates"][1:]
+
         building_part_bottom = gdf.iloc[0]["elevation_at_bottom"]
         building_part_top = gdf.iloc[0]["elevation_at_top"]
         extrude_by = building_part_top - building_part_bottom
-        cad_obj = cad_from_coords(
-            workplane, coordinates, extrude_by=extrude_by
+        
+        cad_obj = solid_from_coords(
+            workplane, coords_exterior_ring, interior_rings, extrude_by=extrude_by
         )
 
-        # translate up on Z axis to level with building part underneath
-        if building_part != "base":
+        if building_part == "base":
+            # create the cutout for the aruco marker
+            cad_obj = cut_marker_placeholder(cad_obj)
+        else:
+            # translate up on Z axis to level with building part underneath
             cad_obj = cad_obj.translate(
                 (0, 0, building_part_bottom)  # + (building_part_height / 2))
             )
-
+        
         exporters.export(cad_obj, building_part + ".stl")
 
         workplane = workplane.union(cad_obj)
@@ -83,11 +93,14 @@ def make_cq_workplane_with_building(building: geopandas.GeoDataFrame()):
     return workplane
 
 
-def cut_marker_placeholder(workplane):
-    placeholder_box = workplane.box(10, 10, 2, centered=True)
-    workplane.cut(placeholder_box)
+def cut_marker_placeholder(solid):
+    # Create the rectangular cutout shape
+    cutout_shape = cq.Workplane("XY").rect(10, 10)
 
-    return workplane
+    # Cut the shape into the bottom of the main solid
+    solid = solid.cut(cutout_shape.extrude(1))
+
+    return solid
 
 
 def cut_text_on_front():
@@ -96,30 +109,25 @@ def cut_text_on_front():
     pass
 
 
-
 if __name__ == "__main__":
-
     gdf = read_geojson_files()
     buildings_ids = list(gdf["building_id"].unique())
 
     count = 0
     for building_id in buildings_ids:
-
         if count < 10:
             count += 1
             continue
-    
+
         building = gdf[gdf["building_id"] == building_id]
 
         cad_obj = make_cq_workplane_with_building(building.copy())
 
         import os
-        
-        exporters.export(cad_obj,  building_id + ".stl")
-        print("exported", cad_obj,  building_id + ".stl")
+
+        exporters.export(cad_obj, building_id + ".stl")
+        print("exported", cad_obj, building_id + ".stl")
         exit()
-
-
 
 
 # grasbrook_gdf = geopandas.read_file("grasbrook.geojson")
