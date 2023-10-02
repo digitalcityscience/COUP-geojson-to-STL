a47983c37d55150a7d1392442c2b0aa8d1ef5d63
Andre@HCU <andre.landwehr@hcu-hamburg.de>
2023-10-02 12:47:54 +0200

    feat: add scaling

--- geojson2stl.py
+++ geojson2stl.py
@@ -4,12 +4,15 @@ import pandas as pd
 from cadquery import exporters
 import json
 import matplotlib.pyplot as plt
+from shapely.affinity import scale
+
 
 # exporters.export(result, "./mesh_test.stl")
 
 files = ["base", "groundfloor", "upperfloor", "rooftops"]
 
 
+# returns all geojson files projected to metric coords in 1 gdf
 def read_geojson_files() -> geopandas.GeoDataFrame():
     gdfs = []
     for filename in files:
@@ -55,6 +58,8 @@ def make_cq_workplane_with_building(building: geopandas.GeoDataFrame()):
         -building_centroid.x, -building_centroid.y
     )
 
+    building.to_file("test_building.geojson", driver="GeoJSON")
+
     workplane = cq.Workplane("XY")
 
     for building_part in files:
@@ -71,8 +76,8 @@ def make_cq_workplane_with_building(building: geopandas.GeoDataFrame()):
 
         building_part_bottom = gdf.iloc[0]["elevation_at_bottom"]
         building_part_top = gdf.iloc[0]["elevation_at_top"]
-        extrude_by = building_part_top - building_part_bottom
-        
+        extrude_by = (building_part_top - building_part_bottom)
+
         cad_obj = solid_from_coords(
             workplane, coords_exterior_ring, interior_rings, extrude_by=extrude_by
         )
@@ -85,17 +90,17 @@ def make_cq_workplane_with_building(building: geopandas.GeoDataFrame()):
             cad_obj = cad_obj.translate(
                 (0, 0, building_part_bottom)  # + (building_part_height / 2))
             )
-        
+
         exporters.export(cad_obj, building_part + ".stl")
 
         workplane = workplane.union(cad_obj)
-
+    
     return workplane
 
 
 def cut_marker_placeholder(solid):
     # Create the rectangular cutout shape
-    cutout_shape = cq.Workplane("XY").rect(10, 10)
+    cutout_shape = cq.Workplane("XY").rect(20, 20)
 
     # Cut the shape into the bottom of the main solid
     solid = solid.cut(cutout_shape.extrude(1))
@@ -103,57 +108,36 @@ def cut_marker_placeholder(solid):
     return solid
 
 
-def cut_text_on_front():
-    # text = workplane.text("Z", 5, -1.0).translate((45, 0, 200))
-    # test = test.faces(">Z").cut(text)
-    pass
+def scale_geometries(gdf, scale_factor: float) -> geopandas.GeoDataFrame():
+    gdf["geometry"] = gdf["geometry"].apply(
+        lambda geom: scale(geom, xfact=scale_factor, yfact=scale_factor)
+    )
+    gdf["building_height"] = gdf["building_height"] * scale_factor
+    gdf["elevation_at_bottom"] = gdf["elevation_at_bottom"] * scale_factor
+    gdf["elevation_at_top"] = gdf["elevation_at_top"] * scale_factor
+
+    return gdf
 
 
 if __name__ == "__main__":
     gdf = read_geojson_files()
+
+    # Scale all geometries by a factor of
+    # 2 (scale of 1/500) *1000, as cadquery works in mm not m)
+    scale_factor = 2
+    gdf = scale_geometries(gdf, scale_factor)
+   
     buildings_ids = list(gdf["building_id"].unique())
 
-    count = 0
     for building_id in buildings_ids:
-        if count < 10:
-            count += 1
-            continue
+        try:
+            building = gdf[gdf["building_id"] == building_id]
 
-        building = gdf[gdf["building_id"] == building_id]
+            cad_obj = make_cq_workplane_with_building(building.copy())
 
-        cad_obj = make_cq_workplane_with_building(building.copy())
-
-        import os
-
-        exporters.export(cad_obj, building_id + ".stl")
-        print("exported", cad_obj, building_id + ".stl")
-        exit()
-
-
-# grasbrook_gdf = geopandas.read_file("grasbrook.geojson")
-# print(type(grasbrook_gdf.iloc[0]))
-
-
-# grasbrook_gdf["centroid"] = grasbrook_gdf.geometry.centroid
-# centroid = grasbrook_gdf.iloc[0]["centroid"]
-# grasbrook_gdf = grasbrook_gdf.translate(-centroid.x, -centroid.y)
-# minx, miny, maxx, maxy = grasbrook_gdf.total_bounds
-
-# print("total bounds", minx, miny, maxx, maxy)
-# grasbrook_gdf.plot()
-
-# gb_json = json.loads(grasbrook_gdf.to_json())
-
-
-# pol = gb_json["features"][0]["geometry"]["coordinates"]
-# pol = pol[0]
-
-# workplane = cq.Workplane("XY")
-# polyline = workplane.polyline(pol).close()
-# result = polyline.extrude(100)
-# test = result.union(workplane.box(100, 100, 100, centered=True).translate((0, 0, 150)))
-# text = workplane.text("Z", 5, -1.0).translate((45, 0, 200))
-
-# test = test.faces(">Z").cut(text)
-# print("hello")
-# display(test)
+            exporters.export(cad_obj, building_id + ".stl")
+            print("exported", cad_obj, building_id + ".stl")
+        except Exception as e:
+            print(f"problems with building {building_id}")
+            print(e)
+            building.to_file("./problems/" + building_id + ".geojson", driver="GeoJSON")
